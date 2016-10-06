fea603cdda05ec0515b49aa21faf9c8e38d2c5c8
zyuiop <zyuiop@zyuiop.net>
2016-10-06 15:51:54 +0200

    Save memes

--- src/main/java/net/zyuiop/discordbot/DiscordEventHandler.java
+++ src/main/java/net/zyuiop/discordbot/DiscordEventHandler.java
@@ -1,6 +1,15 @@
 package net.zyuiop.discordbot;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import net.zyuiop.discordbot.commands.DiscordCommand;
+import org.apache.commons.io.IOUtils;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.impl.events.MessageUpdateEvent;
@@ -16,7 +25,7 @@ public class DiscordEventHandler {
 	@EventSubscriber
 	public void onMessage(MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
 		IMessage message = event.getMessage();
-		CommandRegistry.handle(message);
+		onMessage(message);
 	}
 	@EventSubscriber
 	public void onMessage(MessageUpdateEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
@@ -24,6 +33,22 @@ public class DiscordEventHandler {
 			return;
 
 		IMessage message = event.getNewMessage();
+		onMessage(message);
+	}
+
+	private void onMessage(IMessage message) throws RateLimitException, DiscordException, MissingPermissionsException {
+		if (message.getContent().contains("i.imgflip.com") && message.getAuthor().isBot()) {
+			URL website;
+			try {
+				website = new URL(message.getContent());
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream(new File("memesarchive", website.getFile()));
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 		CommandRegistry.handle(message);
 	}
 }
